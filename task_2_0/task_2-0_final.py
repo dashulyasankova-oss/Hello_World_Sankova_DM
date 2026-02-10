9c3cf5a3d73e19f38ef6ebad0d7725d687a5398d
dashulyasankova-oss <dashulyasankova@gmail.com>
2026-02-10 17:26:50 +0300

    Add files via upload

---
+++ task_2_0/task_2-0_final.py
@@ -0,0 +1,7 @@
+current_year = 2026
+course = 1
+print("=" * 40, end="\n\n")
+print("✨", "Добро пожаловать!", "✨", sep="  ")
+print("🎓", f"Студент {course} курса", current_year, "🎓", sep="  ")
+print("📚", "Удачи в обучении!", "📚", sep="  ", end="\n\n")
+print("=" * 40, end="\n\n")
